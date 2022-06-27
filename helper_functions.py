0bec6cf599bdac70a04df107fa51484b3bffd1fc
chnoma <85920821+chnoma@users.noreply.github.com>
2022-06-27 12:52:41 -0500

    Added DataEntry class Added Shipment class Added Type/Project flag constants for use with the DataEntry class Added basic interface functionality
    Added helper_functions.py

---
+++ helper_functions.py
@@ -0,0 +1,85 @@
+import pandas as pd
+
+
+# region Constants
+SITE_DETAILS = pd.read_excel("./settings/SiteList.xlsx")
+SITE_DETAILS.set_index("Station#", inplace=True)
+# endregion
+
+
+def jam(d):  # If string is None or nan, return ""
+    o = str(d)
+    if o == "None" or o == "nan":
+        return ""
+    return o
+
+
+def code_to_site(site_code, area):
+    output = {}
+    multi = False
+    # THIS SECTION FINDS DUPLICATE VA ID'S AND USES THE PROVIDED NAME TO DECONFLICT THEM AND FIND THE RIGHT ONE
+    if isinstance(SITE_DETAILS["District"][site_code], pd.core.series.Series):
+        for k, v in enumerate(SITE_DETAILS["District"][site_code].reset_index()):
+            if area == SITE_DETAILS["Area"][site_code].reset_index()["Area"][k]:
+                multi = True
+                output["District"] = jam(SITE_DETAILS["District"][site_code].reset_index()["District"][k])
+                output["Area"] = jam(SITE_DETAILS["Area"][site_code].reset_index()["Area"][k])
+                output["MyVA VISN"] = jam(SITE_DETAILS["MyVA VISN"][site_code].reset_index()["MyVA VISN"][k])
+                output["Area"] = jam(SITE_DETAILS["Area"][site_code].reset_index()["Area"][k])
+                output["Site"] = jam(site_code)
+                output["Location Code"] = jam(
+                    SITE_DETAILS["Location Code"][site_code].reset_index()["Location Code"][k])
+                output["Shipping Address"] = jam(
+                    SITE_DETAILS["Shipping Address"][site_code].reset_index()["Shipping Address"][k])
+                output["Shipping City"] = jam(
+                    SITE_DETAILS["Shipping City"][site_code].reset_index()["Shipping City"][k])
+                output["Shipping State"] = jam(
+                    SITE_DETAILS["Shipping State"][site_code].reset_index()["Shipping State"][k])
+                output["Shipping Zip Code"] = jam(
+                    SITE_DETAILS["Shipping Zip Code"][site_code].reset_index()["Shipping Zip Code"][k])
+                output["E-mail Distribution List for Logistics"] = jam(
+                    SITE_DETAILS["E-mail Distribution List for Logistics"][site_code].reset_index()[
+                        "E-mail Distribution List for Logistics"][k])
+                output["E-mail Distribution List for OIT"] = jam(
+                    SITE_DETAILS["E-mail Distribution List for OIT"][site_code].reset_index()[
+                        "E-mail Distribution List for OIT"][k])
+                output["Delivery POC"] = jam(
+                    SITE_DETAILS["Delivery POC"][site_code].reset_index()["Delivery POC"][k])
+                output["Delivery POC Cell Phone#"] = jam(
+                    SITE_DETAILS["Delivery POC Cell Phone#"][site_code].reset_index()["Delivery POC Cell Phone#"][k])
+                output["Delivery POC Email"] = jam(
+                    SITE_DETAILS["Delivery POC Email"][site_code].reset_index()["Delivery POC Email"][k])
+                output["Delivery POC Phone#"] = jam(
+                    SITE_DETAILS["Delivery POC Phone#"][site_code].reset_index()["Delivery POC Phone#"][k])
+                output["Alternate POC"] = jam(
+                    SITE_DETAILS["Alternate POC"][site_code].reset_index()["Alternate POC"][k])
+                output["Alternate POC Cell Phone#"] = jam(
+                    SITE_DETAILS["Alternate POC Cell Phone#"][site_code].reset_index()["Alternate POC Cell Phone#"][k])
+                output["Alternate POC Email"] = jam(
+                    SITE_DETAILS["Alternate POC Email"][site_code].reset_index()["Alternate POC Email"][k])
+                output["Alternate POC Phone#"] = jam(
+                    SITE_DETAILS["Alternate POC Phone#"][site_code].reset_index()["Alternate POC Phone#"][k])
+    if not multi:
+        output["District"] = jam(SITE_DETAILS["District"][site_code])
+        output["Area"] = jam(SITE_DETAILS["Area"][site_code])
+        output["MyVA VISN"] = jam(SITE_DETAILS["MyVA VISN"][site_code])
+        output["Area"] = jam(SITE_DETAILS["Area"][site_code])
+        output["Site"] = jam(site_code)
+        output["Location Code"] = jam(SITE_DETAILS["Location Code"][site_code])
+        output["Shipping Address"] = jam(SITE_DETAILS["Shipping Address"][site_code])
+        output["Shipping City"] = jam(SITE_DETAILS["Shipping City"][site_code])
+        output["Shipping State"] = jam(SITE_DETAILS["Shipping State"][site_code])
+        output["Shipping Zip Code"] = jam(SITE_DETAILS["Shipping Zip Code"][site_code])
+        output["E-mail Distribution List for Logistics"] = jam(
+            SITE_DETAILS["E-mail Distribution List for Logistics"][site_code])
+        output["E-mail Distribution List for OIT"] = jam(
+            SITE_DETAILS["E-mail Distribution List for OIT"][site_code])
+        output["Delivery POC"] = jam(SITE_DETAILS["Delivery POC"][site_code])
+        output["Delivery POC Cell Phone#"] = jam(SITE_DETAILS["Delivery POC Cell Phone#"][site_code])
+        output["Delivery POC Email"] = jam(SITE_DETAILS["Delivery POC Email"][site_code])
+        output["Delivery POC Phone#"] = jam(SITE_DETAILS["Delivery POC Phone#"][site_code])
+        output["Alternate POC"] = jam(SITE_DETAILS["Alternate POC"][site_code])
+        output["Alternate POC Cell Phone#"] = jam(SITE_DETAILS["Alternate POC Cell Phone#"][site_code])
+        output["Alternate POC Email"] = jam(SITE_DETAILS["Alternate POC Email"][site_code])
+        output["Alternate POC Phone#"] = jam(SITE_DETAILS["Alternate POC Phone#"][site_code])
+    return output
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import shutil
 import shelve
 
-import pandas as pd
 from datetime import date
 from dataclasses import dataclass
 
@@ -16,24 +15,40 @@ from PyQt5.QtCore import QFileInfo
 import excelreader
 from fedex_api import FedexAPI
 
+# region Constants
+TYPE_SHIPMENT = 0
+TYPE_DN_REQUEST = 1
+PROJECT_SUPPORTING_TECH = 0
+PROJECT_PVAAS = 1
+PROJECT_SPECIALIZED_DEVICES = 3
+PROJECT_OTHER = 4
+# endregion
+
 
 @dataclass
-class shipmentNotification:
+class Shipment:
+    project: str
+    location_code: str
+    location_name: str
+    ncs_so: str
+    ncs_inv: str
+    tracking_no: str
+    carrier: str
+    ship_date: str
+    delivery_date: str
+
+
+@dataclass
+class DataEntry:
     """Class detailing information about a given shipment notification/missing dn entry"""
     project: str
     title: str
-    is_shipment_notification: bool
+    type: int
     email_generated: bool
     site_code: int
     date_added: date
     date_sent: date
-    uid: int
-
-
-# region Constants
-SITE_DETAILS = pd.read_excel("./settings/SiteList.xlsx")
-SITE_DETAILS.set_index("Station#", inplace=True)
-# endregion
+    data: list  # shipment notification: 0 - XLSX, 1 - PDF | DN Request: array of shipment dataclasses
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -42,9 +57,10 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         uic.loadUi('./ui/main.ui', self)
         self.show()
-        self.shelve = shelve.open("./settings/registry")
+
         self.snowModel = QStandardItemModel()
         self.snowModel.setHorizontalHeaderLabels(["Project/Name", "Type", "Date"])
+
         root = self.snowModel.invisibleRootItem()
         root.appendRow([QStandardItem("Supporting Technologies")])
         root.appendRow([QStandardItem("PVaaS")])
@@ -54,8 +70,47 @@ class MainWindow(QtWidgets.QMainWindow):
         self.snowTreeView.setModel(self.snowModel)
         self.snowTreeView.header().setDefaultSectionSize(200)
 
-    def close(self) -> bool:
+        self.shelve = shelve.open("./settings/registry")
+        if "items" not in self.shelve.keys():
+            self.shelve["items"] = []
+
+        self.purchaseOrderBrowseButton.pressed.connect(self.browse_po)
+        self.purchaseOrderOpenButton.pressed.connect(self.open_po)
+        self.shipmentBrowseButton.pressed.connect(self.browse_xlsx)
+        self.shipmentOpenButton.pressed.connect(self.open_xlsx)
+        self.purchaseOrderLineEdit.textChanged.connect(self.validate_files)
+        self.shipmentLineEdit.textChanged.connect(self.validate_files)
+
+    def close(self):
         self.shelve.close()
+
+    def validate_files(self):
+        self.saveButton.setEnabled(
+            os.path.exists(self.purchaseOrderLineEdit.text()) and os.path.exists(self.shipmentLineEdit.text()))
+
+    def browse_po(self):
+        file = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "Purchase Orders (*.pdf)")[0]
+        if file == "":
+            return
+        self.purchaseOrderLineEdit.setText(file)
+
+    def open_po(self):
+        if not os.path.exists(self.purchaseOrderLineEdit.text()):
+            return
+        else:
+            os.system('"' + self.purchaseOrderLineEdit.text() + '"')
+
+    def browse_xlsx(self):
+        file = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "Excel Files (*.xlsx)")[0]
+        if file == "":
+            return
+        self.shipmentLineEdit.setText(file)
+
+    def open_xlsx(self):
+        if not os.path.exists(self.shipmentLineEdit.text()):
+            return
+        else:
+            os.system('"' + self.shipmentLineEdit.text() + '"')
 
 
 if __name__ == "__main__":
