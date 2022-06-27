a17b228ab7a568c1523d815979de5a82ae8eecb0
chnoma <85920821+chnoma@users.noreply.github.com>
2022-06-27 11:07:38 -0500

    Updated directory structure - added shelve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import shutil
+import shelve
+
 import pandas as pd
 from datetime import date
 from dataclasses import dataclass
@@ -14,10 +16,25 @@ from PyQt5.QtCore import QFileInfo
 import excelreader
 from fedex_api import FedexAPI
 
+
+@dataclass
+class shipmentNotification:
+    """Class detailing information about a given shipment notification/missing dn entry"""
+    project: str
+    title: str
+    is_shipment_notification: bool
+    email_generated: bool
+    site_code: int
+    date_added: date
+    date_sent: date
+    uid: int
+
+
 # region Constants
-SITE_DETAILS = pd.read_excel("SiteList.xlsx")
+SITE_DETAILS = pd.read_excel("./settings/SiteList.xlsx")
 SITE_DETAILS.set_index("Station#", inplace=True)
 # endregion
+
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -25,11 +42,21 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         uic.loadUi('./ui/main.ui', self)
         self.show()
-
+        self.shelve = shelve.open("./settings/registry.db")
         self.snowModel = QStandardItemModel()
         self.snowModel.setHorizontalHeaderLabels(["Project/Name", "Type", "Date"])
+        root = self.snowModel.invisibleRootItem()
+        root.appendRow([QStandardItem("Supporting Technologies")])
+        root.appendRow([QStandardItem("PVaaS")])
+        root.appendRow([QStandardItem("Specialized Devices")])
+        root.appendRow([QStandardItem("Other")])
+
         self.snowTreeView.setModel(self.snowModel)
         self.snowTreeView.header().setDefaultSectionSize(200)
+
+    def close(self) -> bool:
+        self.shelve.close()
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
